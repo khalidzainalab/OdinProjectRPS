2a9c98755e89cb09587ccd6fb138e37ccdfd595b
khalidzainalab <khalidzainalab@gmail.com>
2024-09-05 23:36:11 +0800

    add new function computerChoice to randomly generate an answer
    output Rock Paper Scissors with random generation and pass it on to variable computerChoice

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function getComputerChoice{
-    let computerChoice = ''; /* initalize empty variable to store computerChoice*/
-    let randomNumber = Math.floor(Math.random() * 3) + 1 /* variable to randomize number from 1 - 3 */
-    
-    switch 
+/* global variables that are declared to be passed on to other function scopes */
+
+let computerChoice = ''; /* initalize empty variable to store computerChoice*/
+
+
+
+function getComputerChoice(){
+   
+    switch (Math.floor(Math.random() * 3) + 1) {
+        case 1:
+            computerChoice = "rock";
+            break;
+        case 2:
+            computerChoice = "paper";
+            break;
+        case 3:
+            computerChoice = 'scissors';
+    }
+    return computerChoice;
 } 
+
+console.log(getComputerChoice());
