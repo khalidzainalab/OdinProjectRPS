054ee84edc3ba4e4c10d8244c37c1830dd983d9d
khalidzainalab <khalidzainalab@gmail.com>
2024-09-28 20:31:08 +0800

    show text to results when a user wins or lose

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 /* global variables that are declared to be passed on to other function scopes */
 
-let humanScore= 0;
+let humanScore = 0;
 let computerScore = 0;
 
 /* -- functions -- */
 
-function getComputerChoice(){
+function getComputerChoice() {
     let computerChoice = '';
     switch (Math.floor(Math.random() * 3) + 1) {
         case 1:
@@ -18,36 +18,36 @@ function getComputerChoice(){
             computerChoice = 'scissors';
     }
     return computerChoice;
-} 
+}
 
-function getHumanChoice(){
+function getHumanChoice() {
     let humanChoice = '';
     let userInput = prompt("Rock, Paper or Scissors?", "Rock");
     humanChoice = userInput.toLowerCase();
-    if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors"){
-        
-    }else{
+    if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
+
+    } else {
         userInput = prompt("Invalid selection, must be rock, paper or scissors", "Rock");
         humanChoice = userInput.toLowerCase();
     }
     return humanChoice;
 }
 
-function playRound(humanChoice, computerChoice){
+function playRound(humanChoice, computerChoice) {
     const beatenBy = {
         'rock': 'scissors',
-        'paper' : 'rock',
-        'scissors' : 'paper'
+        'paper': 'rock',
+        'scissors': 'paper'
     };
-   
+
     if (humanChoice === computerChoice) {
-        return console.log(`The game is a draw, computer answered ${computerChoice}. Score is still ${humanScore}-${computerScore}`);
+        return results.textContent = `The game is a draw, computer answered ${computerChoice}. Score is still ${humanScore}-${computerScore}`;
     } else if (beatenBy[humanChoice] === computerChoice) {
         humanScore += 1;
-        return console.log(`You won! computer answered ${computerChoice}. Score is ${humanScore}-${computerScore}`);
+        return results.textContent = `You won! computer answered ${computerChoice}. Score is ${humanScore}-${computerScore}`;
     } else {
         computerScore += 1;
-        return console.log(`You lost :(. Computer answered ${computerChoice}. Score is ${humanScore}-${computerScore}`);
+        return results.textContent = `You lost :(. Computer answered ${computerChoice}. Score is ${humanScore}-${computerScore}`;
     }
 }
 
@@ -69,12 +69,26 @@ function playRound(humanChoice, computerChoice){
 
 const buttons = document.querySelectorAll("button");
 const results = document.querySelector(".results");
+const announcement = document.querySelector('.announcement')
+
 
 buttons.forEach((button) => {
-    button.addEventListener("click" , () => {
+    button.addEventListener("click", () => {
         const humanValue = button.id;
         const computerValue = getComputerChoice();
 
-        return playRound(humanValue, computerValue);   
+        playRound(humanValue, computerValue);
+        console.log(humanScore);
+
+        if (humanScore == 5) {
+            results.textContent = 'You have won! reseting back to 0';
+            humanScore = 0;
+            computerScore = 0;
+        } else if (computerScore == 5) {
+            results.textContent = 'You have lost! reseting back to 0';
+            humanScore = 0;
+            computerScore = 0;
+        }
     });
 });
+
